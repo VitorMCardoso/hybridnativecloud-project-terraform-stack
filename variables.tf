f407da040461207af5135be1d22c9a0fe394b2bb
Vitor Cardoso <vitorcardoso.desenvolvedor@gmail.com>
2020-05-15 14:57:10 -0300

    Ajust ssh path

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "KEY_NAME" {
   default = "nginx-lab-prod"
 }
 variable "PATH_TO_KEY" {
-  default = "/.ssh/nginx-lab.pem"
+  default = "/app/.ssh/nginx-lab.pem"
 }
 variable "INSTANCE_USERNAME" {
   default = "ubuntu"
